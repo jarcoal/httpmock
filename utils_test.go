8770d8787c86f101ae6cdd69d7d8f017e22dcea4
Sam Mulube <sam@umbrellium.co.uk>
2016-06-02 16:26:33 +0100

    Add some tests for our little normalize function

--- utils_test.go
+++ utils_test.go
@@ -6,25 +6,55 @@ import (
 
 func TestNormalizeURL(t *testing.T) {
 	testcases := []struct {
-		input       string
+		inputs      []string
 		expected    string
 		expectedErr bool
 	}{
 		{
-			"http://example.com",
-			"http://example.com",
+			[]string{
+				"www.example.com",
+				"www.example.com:80",
+				"http://www.example.com",
+				"HTTP://WWW.eXamPle.Com:80",
+			},
+			"http://www.example.com",
 			false,
+		},
+		{
+			[]string{
+				"http://www.example.com/a/b/index.html?foo=val&bar=val#t=20",
+				"www.example.com:80/a/b///../x/../../index.html?foo=val&bar=val#t=20",
+			},
+			"http://www.example.com/a/b/index.html?bar=val&foo=val#t=20",
+			false,
+		},
+		{
+			[]string{
+				"<funnytag>",
+				"javascript:evilFunc()",
+				"anotherscheme:garbage",
+			},
+			"",
+			true,
 		},
 	}
 
 	for _, testcase := range testcases {
-		got, err := normalizeURL(testcase.input)
-		if err != nil && !testcase.expectedErr {
-			t.Errorf("Unexpected error, got %#v", err)
-		}
+		for _, input := range testcase.inputs {
+			got, err := normalizeURL(input)
+			if testcase.expectedErr {
+				if err == nil {
+					t.Errorf("Expected error for '%s', got none", input)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Unexpected error, got %#v", err)
+				}
 
-		if got != testcase.expected {
-			t.Errorf("Unexepcted output, expected: %s, got %s", testcase.expected, got)
+				if got != testcase.expected {
+					t.Errorf("Unexepcted output, expected: %s, got %s", testcase.expected, got)
+				}
+			}
 		}
 	}
 }
