jarcoal/httpmock

commit f085181e6a3c84a2dfe6771234129bb1c50b002f

Add sorted query feature (#50)

transport.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"sort"
 	"strings"
 	"sync"
 )
@@ -43,7 +44,17 @@ type MockTransport struct {
 // implement the http.RoundTripper interface.  You will not interact with this directly, instead
 // the *http.Client you are using will call it for you.
 func (m *MockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	queryAsMap := make(map[string]string)
+	for k, v := range map[string][]string(req.URL.Query()) {
+		queryAsMap[k] = v[0]
+	}
+
+	query := mapToSortedQuery(queryAsMap)
 	url := req.URL.String()
+
+	if query != nil {
+		url = strings.Replace(url, req.URL.RawQuery, *query, -1)
+	}
 
 	method := req.Method
 	if method == "" {
@@ -183,6 +194,36 @@ func (m *MockTransport) RegisterResponder(method, url string, responder Responde
 	m.responders[key] = responder
 	m.callCountInfo[key] = 0
 	m.mu.Unlock()
+}
+
+// RegisterResponderWithQuery is same as RegisterResponder, but it doesn't depend on query items order
+func (m *MockTransport) RegisterResponderWithQuery(method, path string, query map[string]string, responder Responder) {
+	url := path
+	queryString := mapToSortedQuery(query)
+	if queryString != nil {
+		url = path + "?" + *queryString
+	}
+	m.RegisterResponder(method, url, responder)
+}
+
+func mapToSortedQuery(m map[string]string) *string {
+	if m == nil {
+		return nil
+	}
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	if len(keys) == 0 {
+		return nil
+	}
+	sort.Strings(keys)
+	var queryArray []string
+	for _, k := range keys {
+		queryArray = append(queryArray, fmt.Sprintf("%s=%s", k, m[k]))
+	}
+	query := strings.Join(queryArray, "&")
+	return &query
 }
 
 // RegisterNoResponder is used to register a responder that will be called if no other responder is
@@ -345,6 +386,26 @@ func DeactivateAndReset() {
 // 		}
 func RegisterResponder(method, url string, responder Responder) {
 	DefaultTransport.RegisterResponder(method, url, responder)
+}
+
+// RegisterResponderWithQuery it is same as RegisterResponder, but doesn't depends on query objects order.
+//
+// Example:
+// 		func TestFetchArticles(t *testing.T) {
+// 			httpmock.Activate()
+// 			httpmock.DeactivateAndReset()
+// 			expecedQuery := map[string]string{
+//				"a": "1",
+//				"b": "2"
+//			}
+//
+// 			httpmock.RegisterResponderWithQuery("GET", "http://example.com/", expecedQuery,
+// 				httpmock.NewStringResponder("hello world", 200))
+//
+//			// requests to http://example.com?a=1&b=2 and http://example.com?b=2&a=1 will now return 'hello world'
+// 		}
+func RegisterResponderWithQuery(method, path string, query map[string]string, responder Responder) {
+	DefaultTransport.RegisterResponderWithQuery(method, path, query, responder)
 }
 
 // RegisterNoResponder adds a mock that will be called whenever a request for an unregistered URL

transport_test.go
@@ -78,7 +78,7 @@ func TestMockTransportDefaultMethod(t *testing.T) {
 
 	RegisterResponder("GET", urlString, NewStringResponder(200, body))
 
-	req := &http.Request {
+	req := &http.Request{
 		URL: url,
 		// Note: Method unspecified (zero-value)
 	}
@@ -437,4 +437,44 @@ func TestMockTransportCallCount(t *testing.T) {
 		t.Fatalf("did not reset the total count of calls correctly. expected it to be 0 after reset, but it was %v", afterResetTotalCallCount)
 	}
 
+}
+
+func TestRegisterResponderWithQuery(t *testing.T) {
+	// create a custom http client w/ custom Roundtripper
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			Dial: (&net.Dialer{
+				Timeout:   60 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 60 * time.Second,
+		},
+	}
+	// activate mocks for the client
+	ActivateNonDefault(client)
+	defer DeactivateAndReset()
+	body := "hello world!"
+	testUrlPath := "http://acme.test/api"
+	expectedQuery := map[string]string{"a": "1", "b": "2"}
+	RegisterResponderWithQuery("GET", testUrlPath, expectedQuery, NewStringResponder(200, body))
+	testFullUrls := []string{testUrlPath + "?a=1&b=2", testUrlPath + "?b=2&a=1"}
+	for _, url := range testFullUrls {
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(data) != body {
+			t.FailNow()
+		}
+	}
 }

commit 275e9df93516fc27a998eca24c8f1cca277ce5bf

Fix httpmock with multiple query params (#54)
This commit addresses an issue that was introduced in commit f085181e6a3c84a2dfe6771234129bb1c50b002f (#50)

transport.go
@@ -44,9 +44,9 @@ type MockTransport struct {
 // implement the http.RoundTripper interface.  You will not interact with this directly, instead
 // the *http.Client you are using will call it for you.
 func (m *MockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	queryAsMap := make(map[string]string)
+	queryAsMap := make(map[string][]string)
 	for k, v := range map[string][]string(req.URL.Query()) {
-		queryAsMap[k] = v[0]
+		queryAsMap[k] = v
 	}
 
 	query := mapToSortedQuery(queryAsMap)
@@ -199,14 +199,18 @@ func (m *MockTransport) RegisterResponder(method, url string, responder Responde
 // RegisterResponderWithQuery is same as RegisterResponder, but it doesn't depend on query items order
 func (m *MockTransport) RegisterResponderWithQuery(method, path string, query map[string]string, responder Responder) {
 	url := path
-	queryString := mapToSortedQuery(query)
+	mapQuery := make(map[string][]string, len(query))
+	for key, e := range query {
+		mapQuery[key] = []string{e}
+	}
+	queryString := mapToSortedQuery(mapQuery)
 	if queryString != nil {
 		url = path + "?" + *queryString
 	}
 	m.RegisterResponder(method, url, responder)
 }
 
-func mapToSortedQuery(m map[string]string) *string {
+func mapToSortedQuery(m map[string][]string) *string {
 	if m == nil {
 		return nil
 	}
@@ -220,7 +224,9 @@ func mapToSortedQuery(m map[string]string) *string {
 	sort.Strings(keys)
 	var queryArray []string
 	for _, k := range keys {
-		queryArray = append(queryArray, fmt.Sprintf("%s=%s", k, m[k]))
+		for _, v := range m[k] {
+			queryArray = append(queryArray, fmt.Sprintf("%s=%s", k, v))
+		}
 	}
 	query := strings.Join(queryArray, "&")
 	return &query

transport_test.go
@@ -158,7 +158,7 @@ func TestMockTransportQuerystringFallback(t *testing.T) {
 	// register the testUrl responder
 	RegisterResponder("GET", testUrl, NewStringResponder(200, "hello world"))
 
-	for _, suffix := range []string{"?", "?hello=world", "?hello=world#foo", "#foo"} {
+	for _, suffix := range []string{"?", "?hello=world", "?hello=world#foo", "?hello=world&hello=all", "#foo"} {
 		reqURL := testUrl + suffix
 
 		// make a request for the testUrl with a querystring
@@ -188,6 +188,9 @@ func TestMockTransportPathOnlyFallback(t *testing.T) {
 		},
 		"/hello/world?query=string": {
 			testUrl + "hello/world?query=string",
+		},
+		"/hello/world?query=string&query=string2": {
+			testUrl + "hello/world?query=string&query=string2",
 		},
 		"/hello/world#fragment": {
 			testUrl + "hello/world#fragment",