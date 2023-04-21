1a2d6be76495343572e3e72a7d2a16ab27d6a5ed
Maxime Soulé <btik-git@scoubidou.com>
2023-04-21 14:56:58 +0200

    refactor: make golangci-lint v1.52.2 happy
    Except for stupid report about "unused-parameter".
    
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- race_test.go
+++ race_test.go
@@ -2,13 +2,20 @@ package httpmock_test
 
 import (
 	"net/http"
+	"sync"
 	"testing"
 
 	. "github.com/jarcoal/httpmock"
 )
 
 func TestActivateNonDefaultRace(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(10)
 	for i := 0; i < 10; i++ {
-		go ActivateNonDefault(&http.Client{})
+		go func() {
+			defer wg.Done()
+			ActivateNonDefault(&http.Client{})
+		}()
 	}
+	wg.Wait()
 }
